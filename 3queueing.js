24a4a66262325d69ca9725e3487b293a73396617
IbraShaheen <engibraheem1995@gmail.com>
2021-05-28 22:36:29 +0300

    Finally it's doneeeee

--- 3queueing.js
+++ 3queueing.js
@@ -1,60 +1,81 @@
-// // not ready yet
+class Node {
+  constructor(groupsize, next = null) {
+    this.groupsize = groupsize;
+    this.next = next;
+  }
+}
 
-// class Node {
-//     constructor(data, nextNode = null) {
-//       this.data = data;
-//       this.nextNode = nextNode;
-//     }
-//   }
+class Queue {
+  constructor(limit = 4) {
+    this.front = null;
+    this.back = null;
+    this.length = 0;
+    this.limit = limit;
+    this.waitingtime = 0;
+  }
 
-//   class Queue {
-//     constructor(limit = 12) {
-//       this.frontNode = null;
-//       this.backNode = null;
-//       this.limit = limit;
-//       this.size = 0;
-//       this.waitingtime = 0;
-//     }
-  
-//     isEmpty = () => this.size === 0;
-  
-//     isFull = () => this.size === this.limit;
-  
-//     peek = () => {
-//       if (this.isEmpty()) console.log("Empty queue!");
-//       else return this.frontNode;
-//     };
-  
-//     enqueue = (data) => {
-//       if (this.isFull()) console.log("There's no place for you here");
-//       else {
-//         const newNode = new Node(data);
-//         if (this.isEmpty()) {
-//           // the new node is both the front and back node
-//           this.frontNode = newNode;
-//           this.backNode = newNode;
-//         } else {
-//           // link the backnode to the new node then make the newnode the backnode
-//           this.backNode.nextNode = newNode;
-//           this.backNode = newNode;
-//         }
-//         this.size++;
-//       }
-//     };
-  
-//     dequeue = () => {
-//       if (this.isEmpty()) {
-//         console.log("OOps! Nothing to remove.");
-//       } else {
-//         const removedNode = this.frontNode;
-//         if (this.size === 1) {
-//           this.frontNode = null;
-//           this.backNode = null;
-//         } else {
-//           this.frontNode = removedNode.nextNode;
-//         }
-//         this.size--;
-//         return removedNode.data;
-//       }
-//     };
-//   }
+  isFull = () => this.length === this.limit;
+
+  isEmpty = () => this.length === 0;
+
+  peek = () =>
+    this.isEmpty()
+      ? `Go ahead no time to wait`
+      : `${this.length} groups of people in front of you. The waiting time ${this.waitingtime} min`
+
+
+  addnode = (groupsize) => {
+    const newNode = new Node(groupsize);
+    if (this.isEmpty()) this.front = newNode;
+    else this.back.next = newNode;
+    this.back = newNode;
+    this.length++;
+    this.waitingtime += groupsize * 0.5;
+    console.log(
+      `${this.length} groups of people in front of you. The waiting time ${this.waitingtime} min`
+    );
+  };
+  enqueue = (groupsize) => {
+    if (this.isFull()) {
+      console.log("queue is full");
+    } else {
+      let people = groupsize;
+      while (people > 12) {
+        this.addnode(12);
+        people -= 12;
+      }
+      this.addnode(people);
+    }
+  };
+
+  dequeue = () => {
+    if (this.isEmpty()) {
+      console.log("Empty queue");
+    } else {
+      const removed = this.front;
+      if (this.length === 1) {
+        this.front = null;
+        this.back = null;
+      } else {
+        this.front = removed.next;
+      }
+      this.length--;
+      this.waitingtime -= removed.groupsize * 0.5;
+      return `\n The front group entered. ${this.length} groups left. The waiting time is ${this.waitingtime} min`;
+    }
+  };
+}
+
+
+const ride = new Queue();
+
+ride.enqueue(2);
+ride.enqueue(6);
+ride.enqueue(14);
+ride.enqueue(6);
+ride.enqueue(10);
+
+//console.log(ride.peek());
+
+console.log(ride.dequeue()); 
+console.log(ride.dequeue()); 
